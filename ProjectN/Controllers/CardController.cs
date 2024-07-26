17bfbf71ca0eaade6fec96e8c7662bf67213f16e
Albert <albert@twjoin.com>
2024-07-26 12:29:44 +0800

    加入專案檔案。

---
+++ ProjectN/Controllers/CardController.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProjectN.Models;
+using System.Reflection.Metadata;
+
+namespace ProjectN.Controllers;
+
+[ApiController]
+[Route("[Controller]")]
+public class CardController : Controller
+{
+    /// <summary>
+    /// 測試用集合
+    /// </summary>
+    private static List<Card> _cards = new();
+
+    /// <summary>
+    /// 取得所有卡片
+    /// </summary>
+    /// <returns></returns>
+    [HttpGet]
+    public List<Card> GetList()
+    {
+        return _cards;
+    }
+
+    /// <summary>
+    /// 取得單張卡片
+    /// </summary>
+    /// <param name="id">卡片編號</param>
+    /// <returns></returns>
+    [HttpGet]
+    [Route("{id}")]
+    public Card GetCard([FromRoute] int id)
+    {
+        return _cards.FirstOrDefault(c => c.Id == id);
+    }
+
+    /// <summary>
+    /// 新增卡片
+    /// </summary>
+    /// <param name="card">參數</param>
+    /// <returns></returns>
+    [HttpPost]
+    public IActionResult AddCard([FromBody] CardParameter card)
+    {
+        var newCard = new Card()
+        {
+            Id = _cards.Any() ? _cards.Max(c => c.Id) + 1 : 0,
+            Name = card.Name,
+            Description = card.Description
+        };
+
+        _cards.Add(newCard);
+
+        return Ok();
+    }
+
+    /// <summary>
+    /// 修改卡片
+    /// </summary>
+    /// <param name="id">CardId</param>
+    /// <param name="cardParam">參數</param>
+    /// <returns></returns>
+    [HttpPut]
+    [Route("{id}")]
+    public IActionResult UpdateCard([FromRoute] int id, [FromBody] CardParameter cardParam)
+    {
+        var card = _cards.FirstOrDefault(c => c.Id == id);
+        if (card == null)
+        {
+            return NotFound();
+        }
+        card.Name = cardParam.Name;
+        card.Description = cardParam.Description;
+        return Ok();
+    }
+
+    /// <summary>
+    /// 刪除卡片
+    /// </summary>
+    /// <param name="id">CardId</param>
+    /// <returns></returns>
+    [HttpDelete]
+    [Route("{id}")]
+    public IActionResult DeleteCard([FromRoute] int id)
+    {
+        var card = _cards.FirstOrDefault(c => c.Id == id);
+
+        if (card == null) 
+        {
+            return NotFound();
+        }
+
+        _cards.RemoveAll(card => card.Id == id);
+
+        return Ok();
+    }
+}
---
+++ ProjectN/Repository/CardRepository.cs
@@ -0,0 +1,81 @@
+﻿using Dapper;
+using Microsoft.Data.SqlClient;
+using Microsoft.IdentityModel.Tokens;
+using ProjectN.Models;
+
+namespace ProjectN.Repository;
+
+/// <summary>
+/// 卡片資料操作
+/// </summary>
+public class CardRepository
+{
+    /// <summary>
+    /// 連線字串
+    /// </summary>
+    private readonly string _connectString = @"Server=(LocalDB)\MSSQLLocalDB;Database=Newbie;Trusted_Connection=True;";
+
+    /// <summary>
+    /// 查詢卡片列表
+    /// </summary>
+    /// <returns></returns>
+    public IEnumerable<Card> GetList()
+    {
+        using (var conn = new SqlConnection(_connectString))
+        {
+            var result = conn.Query<Card>("SELECT * FROM Card");
+            return result;
+        }
+    }
+
+    /// <summary>
+    /// 取得卡片
+    /// </summary>
+    /// <param name="id">卡片id</param>
+    /// <returns></returns>
+    public Card GetCard(int id)
+    {
+        using (var conn = new SqlConnection(_connectString))
+        {
+            var result = conn.QueryFirstOrDefault<Card>("SELECT TOP 1 * FROM Card WHERE Id = @id",
+                new
+                {
+                    Id = id
+                });
+
+            return result;
+        }
+    }
+
+    /// <summary>
+    /// 取得卡片-以參數拆分
+    /// </summary>
+    /// <param name="id">卡片id</param>
+    /// <param name="cost">花費</param>
+    /// <returns></returns>
+    public async Task<Card> GetCardById(int id, int? cost, string? text)
+    {
+        var sql = "SELECT TOP 1 * FROM Card WHERE Id = @id";
+
+        var param = new DynamicParameters();
+        param.Add("Id", id);
+
+        if (cost.HasValue && cost > 1)
+        {
+            sql += "AND Cost = @Cost";
+            param.Add("Cost", cost);
+        }
+
+        if (!text.IsNullOrEmpty())
+        {
+            param.Add("Text", text, System.Data.DbType.AnsiString);
+        }
+
+        using (var conn  = new SqlConnection())
+        {
+            var result = await conn.QueryFirstOrDefaultAsync<Card>(sql, param);
+
+            return result;
+        }
+    }
+}
