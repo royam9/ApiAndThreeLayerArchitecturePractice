afd9406eae48c97312d8ea91f940a99607ecb510
Albert <albert@twjoin.com>
2024-07-31 17:45:52 +0800

    feat: Card CRUD Service, CardApi

---
+++ ProjectN/Controllers/CardApiController.cs
@@ -0,0 +1,125 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProjectN.Models;
+using ProjectN.Repository;
+
+namespace ProjectN.Controllers;
+
+[ApiController]
+[Route("api/[Controller]")]
+public class CardApiController : Controller
+{
+    /// <summary>
+    /// 卡片資料操作
+    /// </summary>
+    private readonly CardRepository _cardRepository;
+
+    public CardApiController()
+    {
+        this._cardRepository = new CardRepository();
+    }
+
+    /// <summary>
+    /// 取得卡片列表
+    /// </summary>
+    /// <returns></returns>
+    [HttpGet]
+    public IActionResult GetList()
+    {
+        return Ok(_cardRepository.GetList());
+    }
+
+    /// <summary>
+    /// 取得卡片
+    /// </summary>
+    /// <param name="id">卡片id</param>
+    /// <returns></returns>
+    [HttpGet]
+    [Route("getCard/{id}")]
+    public IActionResult GetCard([FromRoute] int id)
+    {
+        var result = _cardRepository.GetCard(id);
+
+        if (result == null)
+        {
+            return NotFound();
+        }
+
+        return Ok(result);
+    }
+
+    /// <summary>
+    /// 取得卡片
+    /// </summary>
+    /// <param name="id"></param>
+    /// <param name="cost"></param>
+    /// <param name="text"></param>
+    /// <returns></returns>
+    [HttpGet]
+    [Route("{id}")]
+    public async Task<IActionResult> GetCardById([FromRoute] int id, [FromQuery] int? cost, [FromQuery] string? text)
+    {
+        var result = await _cardRepository.GetCardById(id, cost, text);
+
+        if (result == null)
+        {
+            Response.StatusCode = 404;
+            return NotFound();
+        }
+
+        return Ok(result);
+    }
+
+    /// <summary>
+    /// 新增卡片
+    /// </summary>
+    /// <param name="parameter">參數</param>
+    /// <returns></returns>
+    [HttpPost]
+    public async Task<IActionResult> AddCard([FromBody] CardParameter parameter)
+    {
+        var result = await _cardRepository.AddCard(parameter);
+
+        if (result > 0)
+        {
+            return Ok();
+        }
+
+        return BadRequest();
+    }
+
+    /// <summary>
+    /// 修改卡片
+    /// </summary>
+    /// <param name="id">卡片id</param>
+    /// <param name="parameter">參數</param>
+    /// <returns></returns>
+    [HttpPut]
+    [Route("{id}")]
+    public async Task<IActionResult> UpdateCard([FromRoute] int id, [FromBody] CardParameter parameter)
+    {
+        var result = await _cardRepository.UpdateCard(id, parameter);
+
+        if (result)
+        {
+            return Ok();
+        }
+        else
+        {
+            return BadRequest();
+        }
+    }
+
+    /// <summary>
+    /// 刪除卡片
+    /// </summary>
+    /// <param name="id">卡片id</param>
+    /// <returns></returns>
+    [HttpDelete]
+    [Route("{id}")]
+    public async Task<IActionResult> DeleteCard([FromRoute] int id)
+    {
+        await _cardRepository.DeleteCard(id);
+
+        return Ok();
+    }
+}
--- ProjectN/Repository/CardRepository.cs
+++ ProjectN/Repository/CardRepository.cs
@@ -13,7 +13,7 @@ public class CardRepository
     /// <summary>
     /// 連線字串
     /// </summary>
-    private readonly string _connectString = @"Server=(LocalDB)\MSSQLLocalDB;Database=Newbie;Trusted_Connection=True;";
+    private readonly string _connectString = "Server=Windows-PC-Jack\\SQLEXPRESS;TrustServerCertificate=True;Database=Newbie;Trusted_Connection=True;";
 
     /// <summary>
     /// 查詢卡片列表
@@ -53,14 +53,14 @@ public class CardRepository
     /// <param name="id">卡片id</param>
     /// <param name="cost">花費</param>
     /// <returns></returns>
-    public async Task<Card> GetCardById(int id, int? cost, string? text)
+    public async Task<Card> GetCardById(int id, int? cost = null, string? text = null)
     {
         var sql = "SELECT TOP 1 * FROM Card WHERE Id = @id";
 
         var param = new DynamicParameters();
         param.Add("Id", id);
 
-        if (cost.HasValue && cost > 1)
+        if (cost.HasValue && cost >= 0)
         {
             sql += "AND Cost = @Cost";
             param.Add("Cost", cost);
@@ -71,11 +71,97 @@ public class CardRepository
             param.Add("Text", text, System.Data.DbType.AnsiString);
         }
 
-        using (var conn  = new SqlConnection())
+        using (var conn = new SqlConnection(_connectString))
         {
             var result = await conn.QueryFirstOrDefaultAsync<Card>(sql, param);
-
             return result;
+        }
+    }
+
+    /// <summary>
+    /// 新增卡片
+    /// </summary>
+    /// <param name="parameter">卡片參數</param>
+    /// <returns></returns>
+    public async Task<int> AddCard(CardParameter parameter)
+    {
+        var sql = @"
+                  INSERT INTO Card
+                  (
+                  [Name] 
+                  ,[Description] 
+                  ,[Attack] 
+                  ,[Health] 
+                  ,[Cost]
+                  )
+                  VALUES(
+                  @Name, 
+                  @Description, 
+                  @Attack, 
+                  @Health, 
+                  @Cost);
+                  SELECT SCOPE_IDENTITY();";
+
+        using (var conn = new SqlConnection(_connectString))
+        {
+            await conn.OpenAsync();
+
+            using (var transaction = await conn.BeginTransactionAsync())
+            {
+                var result = await conn.QueryFirstOrDefaultAsync<int>(sql, parameter, transaction);
+                await transaction.CommitAsync();
+                return result;
+            }
+        }
+    }
+
+    /// <summary>
+    /// 修改卡片
+    /// </summary>
+    /// <param name="id">卡片id</param>
+    /// <param name="parameter">卡片參數</param>
+    /// <returns></returns>
+    public async Task<bool> UpdateCard(int id, CardParameter parameter)
+    {
+        var sql = @"UPDATE Card SET 
+                    [Name] = @Name, 
+                    [Description] = @Description,                    
+                    [Attack] = @Attack, 
+                    [Health] = @Health, 
+                    [Cost] = @Cost
+                    WHERE Id = @Id";
+
+        var param = new DynamicParameters(parameter);
+        param.Add("Id", id, System.Data.DbType.Int32);
+
+        using (var conn = new SqlConnection(_connectString))
+        {
+            using (var transaction = conn.BeginTransaction())
+            {
+                var result = await conn.ExecuteAsync(sql, param);
+
+                transaction.Commit();
+
+                return result > 0;
+            }
+        }
+    }
+
+    /// <summary>
+    /// 刪除卡片
+    /// </summary>
+    /// <param name="id">卡片id</param>
+    /// <returns></returns>
+    public async Task DeleteCard(int id)
+    {
+        var sql = @"DELETE FROM Card WHERE Id = @id";
+
+        var param = new DynamicParameters();
+        param.Add("Id", id);
+
+        using (var connection = new SqlConnection(_connectString))
+        {
+            await connection.ExecuteAsync(sql, param);
         }
     }
 }
