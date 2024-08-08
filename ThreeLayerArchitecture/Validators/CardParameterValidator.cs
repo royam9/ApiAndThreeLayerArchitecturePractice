3fd702b308ca7ff546d9760beaf6d940039a08a4
Albert <albert@twjoin.com>
2024-08-08 18:01:43 +0800

    feat: FluentValidation

---
+++ ThreeLayerArchitecture/Validators/CardParameterValidator.cs
@@ -0,0 +1,23 @@
+﻿using FluentValidation;
+using ThreeLayerArchitecture.Model;
+
+namespace ThreeLayerArchitecture.Validators;
+
+/// <summary>
+/// Card Parameter 驗證器
+/// </summary>
+public class CardParameterValidator : AbstractValidator<CardParameter>
+{
+    public CardParameterValidator()
+    {
+        this.RuleFor(card => card.Attack)
+            .GreaterThanOrEqualTo(0);
+
+        // 串列
+        this.RuleForEach(card => card.Name)
+            .NotEmpty();
+
+        this.RuleFor(card => card.Attack)
+            .Must(attack => attack >= 0 && attack <= 3000);
+    }
+}
